a8b148eed8d1a211d5cf006e86c2479eb09deebf
Maneesh-Arora <maneesh2000@yahoo.com>
2022-06-27 18:23:13 +0530

    Fix the documentation and generate index.html correctly.

--- target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
+++ target/generated-sources/annotations/guru/springframework/sfgrestdocsexample/web/mappers/BeerMapperImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-06-27T17:38:31+0530",
+    date = "2022-06-27T18:12:31+0530",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.12 (Amazon.com Inc.)"
 )
 @Component
